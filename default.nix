54caabaa86cba5103be701f245accc17f850d96a
Luchang Jin <ljin.luchang@gmail.com>
2026-02-10 11:01:38 -0500

    small

--- default.nix
+++ default.nix
@@ -1,3 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  version ? "25.11",
+  pkgs ? import (fetchTarball "https://channels.nixos.org/nixos-${version}/nixexprs.tar.xz") {},
+}:
 
 pkgs.callPackage ./notations.nix { }
